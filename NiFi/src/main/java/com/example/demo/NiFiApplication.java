fe2b0f333ffcf071e9f24f4e98f13db2a1fd4342
younsik.choi <nadays3019@gmail.com>
2024-12-20 10:44:30 +0900

    test 깃 등록 ㄷ조ㅗㅁ

---
+++ NiFi/src/main/java/com/example/demo/NiFiApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NiFiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NiFiApplication.class, args);
+	}
+
+}
