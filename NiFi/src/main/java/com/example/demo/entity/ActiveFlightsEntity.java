97c5e61958533b3256160dd10471d462f59c9cd9
dkseo <dkseo@DESKTOP-DIH2VAL>
2024-12-24 17:08:13 +0900

    Lombok 적용

--- NiFi/src/main/java/com/example/demo/entity/ActiveFlightsEntity.java
+++ NiFi/src/main/java/com/example/demo/entity/ActiveFlightsEntity.java
@@ -2,6 +2,9 @@ package com.example.demo.entity;
 import java.sql.Timestamp;
 import java.util.Date;
 
+import lombok.Data;
+
+@Data
 public class ActiveFlightsEntity {
 	
 	private Long id;
@@ -11,48 +14,5 @@ public class ActiveFlightsEntity {
 	private String airlinename;
 	private String flightnumber;
 	private Timestamp inserttimestamp;
-	
-	public Long getId() {
-		return id;
-	}
-	public void setId(Long id) {
-		this.id = id;
-	}
-	public Date getFlight_date() {
-		return flight_date;
-	}
-	public void setFlight_date(Date flight_date) {
-		this.flight_date = flight_date;
-	}
-	public String getFlight_status() {
-		return flight_status;
-	}
-	public void setFlight_status(String flight_status) {
-		this.flight_status = flight_status;
-	}
-	public String getArrgate() {
-		return arrgate;
-	}
-	public void setArrgate(String arrgate) {
-		this.arrgate = arrgate;
-	}
-	public String getAirlinename() {
-		return airlinename;
-	}
-	public void setAirlinename(String airlinename) {
-		this.airlinename = airlinename;
-	}
-	public String getFlightnumber() {
-		return flightnumber;
-	}
-	public void setFlightnumber(String flightnumber) {
-		this.flightnumber = flightnumber;
-	}
-	public Timestamp getInserttimestamp() {
-		return inserttimestamp;
-	}
-	public void setInserttimestamp(Timestamp inserttimestamp) {
-		this.inserttimestamp = inserttimestamp;
-	}
 	
 }
